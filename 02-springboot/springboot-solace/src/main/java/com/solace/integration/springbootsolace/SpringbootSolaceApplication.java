19d6f7a2c4fc85f61958cef36f4bdbc78575682f
Parthiban Natarajan <smile.parthiban@gmail.com>
2023-04-01 10:48:37 +0530

    done

---
+++ 02-springboot/springboot-solace/src/main/java/com/solace/integration/springbootsolace/SpringbootSolaceApplication.java
@@ -0,0 +1,13 @@
+package com.solace.integration.springbootsolace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSolaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSolaceApplication.class, args);
+	}
+
+}
---
+++ 02-springboot/springboot-solace/src/test/java/com/solace/integration/springbootsolace/SpringbootSolaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solace.integration.springbootsolace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSolaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
