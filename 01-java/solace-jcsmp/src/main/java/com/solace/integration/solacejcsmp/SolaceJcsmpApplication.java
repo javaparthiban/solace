6507531bf24eb655413ee00b7c668271cf67d515
Parthiban Natarajan <smile.parthiban@gmail.com>
2023-04-01 11:45:03 +0530

    done

---
+++ 01-java/solace-jcsmp/src/main/java/com/solace/integration/solacejcsmp/SolaceJcsmpApplication.java
@@ -0,0 +1,13 @@
+package com.solace.integration.solacejcsmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolaceJcsmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolaceJcsmpApplication.class, args);
+	}
+
+}
---
+++ 01-java/solace-jcsmp/src/test/java/com/solace/integration/solacejcsmp/SolaceJcsmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solace.integration.solacejcsmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolaceJcsmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
